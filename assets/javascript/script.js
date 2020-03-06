1f55bb6c5b8c472afa1d873135763cb49a7a4d84
copiousamounts <nick.hernstedt@gmail.com>
2020-03-06 15:49:00 -0800

    added inital game values and selected UI elements that will be needed

---
+++ assets/javascript/script.js
@@ -0,0 +1,22 @@
+/*
+GAME FUNCTION:
+    - Player must guess a number between a min and max
+    - Player gets a certain amount of guesses
+    - Notify player of guesses remaining
+    - Notify the player of the correcct answer if loose
+    - Let player choose to play again
+*/
+
+//  Game values
+let min = 1,
+  max = 10,
+  winningNum = 2,
+  guessesLeft = 3;
+
+//   UI elements
+const game = document.getGelementById("game"),
+  minNum = document.querySelector(".min-num"),
+  maxNum = document.querySelector("max-num"),
+  guessBtn = document.querySelector(".#guess-btn"),
+  guessInput = document.querySelector(".#guess-input"),
+  message = document.querySelector(".message");
