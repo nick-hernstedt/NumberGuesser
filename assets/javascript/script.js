68bb03be4f96ed00cb769b80be58bddb2580591b
copiousamounts <nick.hernstedt@gmail.com>
2020-03-09 16:46:55 -0700

    refactored code to not repeat things, set color of win and loss guesses

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -14,9 +14,72 @@ let min = 1,
   guessesLeft = 3;
 
 //   UI elements
-const game = document.getGelementById("game"),
+const game = document.getElementById("game"),
   minNum = document.querySelector(".min-num"),
-  maxNum = document.querySelector("max-num"),
-  guessBtn = document.querySelector(".#guess-btn"),
-  guessInput = document.querySelector(".#guess-input"),
+  maxNum = document.querySelector(".max-num"),
+  guessBtn = document.querySelector("#guess-btn"),
+  guessInput = document.querySelector("#guess-input"),
   message = document.querySelector(".message");
+
+//   Assign UI min and max
+minNum.textContent = min;
+maxNum.textContent = max;
+
+// Listen for guess
+guessBtn.addEventListener("click", function() {
+  let guess = parseInt(guessInput.value);
+  console.log(guess);
+  // Validate
+  if (isNaN(guess) || guess < min || guess > max) {
+    setMessage(`Please enter a number between ${min} and ${max}`, "red");
+  }
+
+  // Check if won
+  if (guess === winningNum) {
+    // Game over - won
+    gameOver(true, `${winningNum} is correct, YOU WIN!`);
+  } else {
+    // Wrong number
+    guessesLeft -= 1;
+
+    if (guessesLeft === 0) {
+      // Game Over - lost
+      gameOver(
+        false,
+        `Game Over, you lost. The correct number was ${winningNum}`
+      );
+    } else {
+      // Game continues - answer wrong
+      guessInput.style.borderColor = "red";
+
+      // Red messgae
+      message.style.color = "red";
+
+      // Clear Input
+      guessInput.value = "";
+
+      // Tell user its the wrong number
+      setMessage(`${guess} is not correct, ${guessesLeft} guesses left`);
+    }
+  }
+});
+
+// Game OVer
+function gameOver(won, msg) {
+  let color;
+  won === true ? (color = "green") : (color = "red");
+  // Disable input
+  guessInput.disabled = true;
+  // Change border color
+  guessInput.style.borderColor = color;
+  // Set text color
+  message.style.color = color;
+  // Set Message
+  setMessage(msg);
+}
+
+// Set message
+function setMessage(msg, color) {
+  message.style.color = color;
+  message.textContent = msg;
+}
